f23ffe922390ddefd04f859a0b205c363ef71cb7
Jacob Lewallen <jlewallen@gmail.com>
2018-01-11 16:31:16 -0800

    Set time on readings.

--- firmware/module/readings.cpp
+++ firmware/module/readings.cpp
@@ -75,6 +75,7 @@ TaskEval WeatherReadings::task() {
 
         for (auto i = 0; i < 12; ++i) {
             readings[i].status = SensorReadingStatus::Done;
+            readings[i].time = clock.getTime();
         }
     }
 
